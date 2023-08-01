21d776eada7cd062878c311dc7666f92b240eeda
tarkIlJun <tack89826@gmail.com>
2023-08-01 10:44:24 +0900

    CRUD3~파일업로드까지.

---
+++ src/main/java/org/astro/aboard3/Aboard3Application.java
@@ -0,0 +1,13 @@
+package org.astro.aboard3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aboard3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aboard3Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/astro/aboard3/Aboard3ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.astro.aboard3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aboard3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
